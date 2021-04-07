8ef4f1db25effd727795e652f600f3224555536a
MariaAntoniaMarín <maamaringr@unal.edu.co>
2021-04-07 15:30:25 -0500

    parallel variables and swapping variables

---
+++ getting started/parallel_variables_and_swapping_variables.rb
@@ -0,0 +1,20 @@
+# Parallel variable assignment 
+=begin
+a = 10
+b = 20
+c = 30
+=end
+a, b, c = 10, 20, 30
+
+p a, b, c
+
+# Swapping variables 
+
+a = "Hello"
+b = "Word"
+
+p a, b
+
+a, b = b, a
+
+p a, b
