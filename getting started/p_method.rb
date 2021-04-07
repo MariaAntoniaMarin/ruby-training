281522d64ff739649bf34a0d60479a5a2057bdea
MariaAntoniaMarín <maamaringr@unal.edu.co>
2021-04-07 13:08:17 -0500

    p method training

---
+++ getting started/p_method.rb
@@ -0,0 +1,9 @@
+puts "Steven Senegal"
+p "Steven Senegal"
+
+# puts method apply the line break and p method indicate the line break
+puts "Hi there, this is a 
+big line break"
+
+p "Hi there, this is a 
+big line break"
