7b84e2055bf210b8cffa92c6e81134dd040397b2
MariaAntoniaMarín <maamaringr@unal.edu.co>
2021-04-07 16:54:44 -0500

    Numbers

---
+++ numbers and booleans/convert_numbers_to_string.rb
@@ -0,0 +1,13 @@
+# String
+str = "5.8"
+p str
+p str.class
+
+# To number
+p str.to_i.class
+p str.to_f
+
+# To string 
+number = 10
+p number.class
+p number.to_s
---
+++ numbers and booleans/intro_to_numbers.rb
@@ -0,0 +1,8 @@
+# Numbers
+p 5
+p 2.45 #float
+
+# All in ruby are objects
+p 5.class
+p 2.75.class
+p 999999999999999999999999999999999999.class
