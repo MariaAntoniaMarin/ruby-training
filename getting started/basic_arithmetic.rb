e05c0c83dd921e37a27afc790ee711b87e88c496
MariaAntoniaMarín <maamaringr@unal.edu.co>
2021-04-07 13:36:43 -0500

    Operators of ruby basic arithmetic

---
+++ getting started/basic_arithmetic.rb
@@ -0,0 +1,22 @@
+# Addition operator
+p 1 + 4
+
+# Minus operator
+p 10 - 6
+
+# Multiplication operator 
+p 3 * 4
+
+# Division operator
+# -Integer division
+p 12 / 5
+
+# -Float division
+p 12.0 / 5
+p 12 / 5.0
+
+# -Modulus operation
+p 13 % 5
+
+# Exponent operator
+p 2 ** 4
