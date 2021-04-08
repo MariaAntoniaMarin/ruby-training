05efe7fdd13c6f45f38536dd8e9005dd4a8beb79
MariaAntoniaMarín <maamaringr@unal.edu.co>
2021-04-08 16:14:35 -0500

    Methods, local variables and parameters & arguments

---
+++ Methods and conditionals I/parameters_and_arguments.rb
@@ -0,0 +1,9 @@
+def praise_person(name, age)
+    puts "#{name} is amazing"
+    puts "#{name} is charming"
+    puts "#{name} is talented"
+    puts "His age is #{age}"
+    puts "His age in 5 years will be #{age + 5}"
+end
+
+praise_person("Antonia", 22)
