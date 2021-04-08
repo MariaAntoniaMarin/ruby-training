e648aff17f1ed44b244dc3e9f0a8b6d7d01679d1
MariaAntoniaMarín <maamaringr@unal.edu.co>
2021-04-08 08:08:21 -0500

    Booleans and odd & even methods

---
+++ numbers and booleans/intro_to_booleans.rb
@@ -0,0 +1,8 @@
+# Booleans
+p 5 < 10 # return a boolean object
+
+awasome = true
+stupid = false
+
+p awasome.class
+p stupid.class
---
+++ numbers and booleans/odd_and_even_methods.rb
@@ -0,0 +1,7 @@
+# odd?
+p 10.odd? # false
+p 11.odd? # true
+
+# even?
+p 2.even? # true
+p 3.even? # false
