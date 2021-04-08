e24dc1801270bdb57e11862ce90f738262bc3332
MariaAntoniaMarín <maamaringr@unal.edu.co>
2021-04-08 08:26:55 -0500

    Equality, inequality, less than, and greater than operators

---
+++ numbers and booleans/inequality_operator.rb
@@ -0,0 +1,9 @@
+# Comparison with the inequality operator
+# Inequality operator is '!='
+p 10 != 5 # true because is not equal
+p 10 != 10 # false because is equal
+
+p "Hello" != "hello" # is ot equal beacause the lower case 
+p "Hello".downcase != "heLlo".downcase
+
+p "123" != 123 # true
---
+++ numbers and booleans/less_greater_opeators.rb
@@ -0,0 +1,11 @@
+# Mathematical conditions
+# Less than and greater than operators
+p 1 < 8 # true
+p 12 < 5 # false
+
+p 5 <= 5 # true
+p 5 < 5 # false
+
+# With floats numbers
+p 12.50 < 12.78 # true
+p 7.43 > 7.44 # false
