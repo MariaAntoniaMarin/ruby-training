c4377f3006d2b295a24cc727e75724c828039408
MariaAntoniaMarín <maamaringr@unal.edu.co>
2021-04-09 15:13:13 -0500

    Arrays - introduction and creation

---
+++ arrays/introduction.rb
@@ -0,0 +1,11 @@
+=begin
+ARRAY
+* An array is an ordered collection of objects. It can be thought of as a list.
+* The array itself is an object. It acts as a container for other objects.
+* An array is created with a pair of square brackets
+=end
+
+p [] #=> [] (empty array)
+p [].class #=> Array
+
+numbers = []
