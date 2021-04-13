8bee3bb25fce70bbc7f8c4a79928a621fbb7d7bd
MariaAntoniaMarín <maamaringr@unal.edu.co>
2021-04-13 17:47:28 -0500

    Request changes applied - fixed indentation

--- strings II/methods/join.rb
+++ strings II/methods/join.rb
@@ -9,8 +9,8 @@ def custom_join(array, delimiter = "")
   string = ""
   last_index = array.length - 1
   array.each_with_index do |elem, index|
-      string << elem
-      string << delimiter unless index == array.length - 1
+    string << elem
+    string << delimiter unless index == array.length - 1
   end
   string
 end
