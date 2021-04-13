0189b37b9cebb5ba5182db30c6ec2780ea8a6a3e
MariaAntoniaMarín <maamaringr@unal.edu.co>
2021-04-13 00:50:29 -0500

    Hashes - word frequency challenge

---
+++ hashes/challenge.rb
@@ -0,0 +1,12 @@
+# Word Frequency
+
+sentence = "Once upon a time in a land far far far away"
+
+def word_count(string)
+  words = string.split(" ")
+  count = Hash.new(0)
+  words.each { |word| count[word] += 1 }
+  count
+end
+
+p word_count(sentence) #=> {"Once"=>1, "upon"=>1, "a"=>2, "time"=>1, "in"=>1, "land"=>1, "far"=>3, "away"=>1}
