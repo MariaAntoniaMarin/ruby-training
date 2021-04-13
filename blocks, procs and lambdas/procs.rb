949de91d875917dccc04e68daeb05b501fcf8e2c
MariaAntoniaMarín <maamaringr@unal.edu.co>
2021-04-13 09:16:43 -0500

    Procs

---
+++ blocks, procs and lambdas/procs.rb
@@ -0,0 +1,40 @@
+cubes = Proc.new { |number| number ** 3 }
+squares = Proc.new { |number| number ** 2 }
+
+a = [1, 2, 3, 4, 5]
+b = [6, 7, 8, 9, 10]
+c = [11, 12, 13, 14, 15]
+
+a_cubes = a.map(&cubes)
+b_cubes = b.map(&cubes)
+c_cubes = c.map(&cubes)
+
+a_squares, b_squares, c_squares = [a, b, c].map { |array| array.map(&squares) }
+
+p a_cubes #=> [1, 8, 27, 64, 125]
+p b_cubes #=> [216, 343, 512, 729, 1000]
+p c_cubes #=> [1331, 1728, 2197, 2744, 3375]
+
+p a_squares #=> [1, 4, 9, 16, 25]
+p b_squares #=> [36, 49, 64, 81, 100]
+p c_squares #=> [121, 144, 169, 196, 225]
+
+# Use procs in a real life situation.
+currencies = [10, 20, 30, 40, 50]
+
+to_euros = Proc.new { |currency| currency * 0.95 }
+to_repees = Proc.new { |currency| currency *  68.13 }
+to_pesos = Proc.new { |currency| currency * 20.67 }
+
+p currencies.map(&to_euros) #=> [9.5, 19.0, 28.5, 38.0, 47.5]
+p currencies.map(&to_repees) #=> [681.3, 1362.6, 2043.8999999999999, 2725.2, 3406.5]
+p currencies.map(&to_pesos) #=> [206.70000000000002, 413.40000000000003, 620.1, 826.8000000000001, 1033.5]
+
+ages = [10, 60, 92, 30, 43, 30]
+
+is_old = Proc.new do |age|
+  age > 60
+end
+
+p ages.select(&is_old) #=> [92]
+p ages.reject(&is_old) #=> [10, 60, 30, 43, 30]
